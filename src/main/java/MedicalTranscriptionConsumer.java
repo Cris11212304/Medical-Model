a8525977e77265862274e311b98a47bd01f88857
Cristian <pasanteinnovacion@bvc.com.co>
2023-10-01 18:55:15 -0500

    Ejecución de Consumer y Producer

--- src/main/java/MedicalTranscriptionConsumer.java
+++ src/main/java/MedicalTranscriptionConsumer.java
@@ -16,6 +16,7 @@ public class MedicalTranscriptionConsumer {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // Crear el consumidor
         Consumer<String, String> consumer = new KafkaConsumer<>(props);
